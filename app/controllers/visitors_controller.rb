13f265ea0d28aa9899bb4a6c66f37039c52daad9
pkuwsjcoding <mrawsj@gmail.com>
2016-11-21 12:36:36 +0000

    update layout application

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,8 @@
 class VisitorsController < ApplicationController
     def new
+        flash.now[:notice]="Welcome"
         @owner=Owner.new
         @currenttime=Date.today
+        flash.now[:alert]="My birthday is soon"
     end 
 end 
