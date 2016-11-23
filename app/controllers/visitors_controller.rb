1878aecf0ff719a3bdf1cbbb679ee87fe6aff16e
pkuwsjcoding <mrawsj@gmail.com>
2016-11-23 12:49:48 +0000

    front end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,8 +1,8 @@
 class VisitorsController < ApplicationController
     def new
-        flash.now[:notice]="Welcome"
+        #flash.now[:notice]="Welcome"
         @owner=Owner.new
         @currenttime=Date.today
-        flash.now[:alert]="My birthday is soon"
+        #flash.now[:alert]="My birthday is soon"
     end 
 end 
