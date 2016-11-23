ac3b793ed0000db3d6a7d684fce298329a315853
pkuwsjcoding <mrawsj@gmail.com>
2016-11-23 22:03:02 +0000

    add contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     #root to: redirect('/about.html')
+  #post "contact", to: 'contacts#process_form'
+  resources :contacts, only: [:new, :create]
   root to: 'visitors#new'
 end
+
+
