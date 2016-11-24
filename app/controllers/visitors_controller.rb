ba286d2c7fa134e48f1733afd625656fbe3182c4
pkuwsjcoding <mrawsj@gmail.com>
2016-11-24 18:45:49 +0000

    add vistor signup form

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,8 +1,25 @@
 class VisitorsController < ApplicationController
     def new
         #flash.now[:notice]="Welcome"
-        @owner=Owner.new
-        @currenttime=Date.today
+        #@owner=Owner.new
+        #@currenttime=Date.today
         #flash.now[:alert]="My birthday is soon"
+        @visitor=Visitor.new
     end 
+    
+    def create
+        @visitor = Visitor.new(secure_params)
+        if @visitor.valid?
+            @visitor.subscribe
+            flash[:notice] = "Signed up #{@visitor.email}."
+            redirect_to root_path
+        else
+            render :new
+        end
+    end 
+    
+    private
+        def secure_params
+            params.require(:visitor).permit(:email)
+        end
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
     #root to: redirect('/about.html')
   #post "contact", to: 'contacts#process_form'
   resources :contacts, only: [:new, :create]
+  resources :visitors, only: [:new, :create]
   root to: 'visitors#new'
-end
+end 
 
 
