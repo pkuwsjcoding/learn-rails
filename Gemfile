a7dedba4a424ab13f089266f80b22499d6733d46
pkuwsjcoding <mrawsj@gmail.com>
2016-11-25 02:08:08 +0000

    update heroku

--- Gemfile
+++ Gemfile
@@ -3,8 +3,9 @@ source 'https://rubygems.org'
 ruby '2.3.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
-# Use sqlite3 as the database for Active Record
+
 gem 'sqlite3'
+
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
@@ -60,6 +61,6 @@ group :development do
   gem 'sqlite3'
 end 
   
-group :product do 
+group :production do 
   gem 'pg'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,13 +87,13 @@ Rails.application.configure do
   
   # email enabled in production
 config.action_mailer.smtp_settings = {
-address: "smtp.sendgrid.net",
-port: 2525,
-domain: Rails.application.secrets.domain_name,
-authentication: "plain",
-enable_starttls_auto: true,
-user_name: Rails.application.secrets.email_provider_username,
-password: Rails.application.secrets.email_provider_password
+  address: "smtp.sendgrid.net",
+  port: 2525,
+  domain: Rails.application.secrets.domain_name,
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: Rails.application.secrets.email_provider_username,
+  password: Rails.application.secrets.email_provider_password
 }
 # ActionMailer Config
 config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
